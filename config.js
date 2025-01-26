3218541371cdd577ae03d6d7e2df3656b25d611f
Will <125048472+Huntwi12@users.noreply.github.com>
2025-01-26 15:23:21 -0500

    Part 1 Of the Revision
    New Design

---
+++ config.js
@@ -0,0 +1,69 @@
+window.PorfolioConfig = {
+  name: 'William Hunt',
+  jobTitle: 'Peasant',
+  jobDescription: 'Eat,Sleep,Work,Work,Work Repeat',
+  skills: [
+    {
+      name: 'Drive',
+      description: 'Seems Like a Second Nature'
+    },
+    {
+      name: 'I Can Shake My Eyes',
+      description: 'Self Explainitory'
+    },
+    {
+      name: 'Sleeping',
+      description: 'I do it Naturally'
+    }
+  ],
+  works: [
+    {
+      name: 'Favorite PS1 Games',
+      link: '/1st Project/index.HTML',
+      description: 'Website I made with HTML & CSS took me about 4 hours.'
+    },
+    {
+      name: 'N/A',
+      link: '',
+      description: 'Work In Progress'
+    },
+    {
+      name: 'N/A',
+      link: '',
+      description: 'Work In Progress'
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Fly Fishing',
+      description: 'Steep Learning Curve. Fully Recommend'
+    },
+    {
+      name: 'Video Games',
+      description: 'Its unhealthy'
+    },
+    {
+      name: 'ADHD Hobby Switching',
+      description: 'Its Killin My Pockets'
+    }
+  ],
+  contacts: {
+    email: '',
+    mobileNo: '',
+    phoneNo: '',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/Huntwi12'
+      },
+      {
+        name: 'Youtube',
+        link: 'https://facebook.com/@Will-fo3sf'
+      },
+      {
+        name: 'Steam',
+        link: 'https://steamcommunity.com/id/Huntwi'
+      }
+    ]
+  }
+};
